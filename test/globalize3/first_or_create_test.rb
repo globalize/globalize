39f7dba1fcb0bda74f8052dfd144d97446a7953a
Artur Małecki <seeweer@gmail.com>
2013-04-01 14:06:13 +0200

    Changed syntax to ruby 1.8 #211

--- test/globalize3/first_or_create_test.rb
+++ test/globalize3/first_or_create_test.rb
@@ -7,11 +7,11 @@ class FirstOrCreateTest < Test::Unit::TestCase
     def test_first_or_create_with_translations_and_when_row_exists
       post = Post.create(:title => 'test_title')
 
-      assert_equal Post.with_translations(:en).where(post_translations: {title: 'test_title'}).first_or_create, post
+      assert_equal Post.with_translations(:en).where(:post_translations => {:title => 'test_title'}).first_or_create, post
     end
 
     def test_first_or_create_with_translations_and_when_row_does_not_exist
-      post = Post.with_translations(:en).where(post_translations: {title: 'test_title'}).first_or_create
+      post = Post.with_translations(:en).where(:post_translations => {:title => 'test_title'}).first_or_create
 
       assert_equal 'test_title', post.title
     end
