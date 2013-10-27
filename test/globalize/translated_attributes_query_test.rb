19790f278698fa454bee37d9732e9a7a3f60b6b7
Chris Salzberg <chrissalzberg@gmail.com>
2013-10-27 15:35:26 +0900

    only run where_values_hash test in Rails >= 3.2.1.

--- test/globalize/translated_attributes_query_test.rb
+++ test/globalize/translated_attributes_query_test.rb
@@ -3,11 +3,13 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class TranslatedAttributesQueryTest
   class WhereValuesHashTest < Test::Unit::TestCase
-    test 'includes translated attributes' do
-      where_values = User.with_translations(:en).where(:email => 'foo@example.com', :user_translations => {:name => 'test_name'}).where_values_hash
+    if ::ActiveRecord::VERSION::STRING >= "3.2.1"
+      test 'includes translated attributes' do
+        where_values = User.with_translations(:en).where(:email => 'foo@example.com', :user_translations => {:name => 'test_name'}).where_values_hash
 
-      assert_equal 'foo@example.com', where_values[:email]
-      assert_equal 'test_name', where_values[:name]
+        assert_equal 'foo@example.com', where_values[:email]
+        assert_equal 'test_name', where_values[:name]
+      end
     end
   end
 end
