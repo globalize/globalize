40a2e367d16ddbc5e3230a93e035b68a4f6f59f0
Chris Salzberg <chrissalzberg@gmail.com>
2013-10-27 16:06:31 +0900

    fix failing test in ruby 1.8.7.

--- test/globalize/translated_attributes_query_test.rb
+++ test/globalize/translated_attributes_query_test.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class TranslatedAttributesQueryTest
-  class WhereValuesHashTest < Test::Unit::TestCase
-    if ::ActiveRecord::VERSION::STRING >= "3.2.1"
+  if ::ActiveRecord::VERSION::STRING >= "3.2.1"
+    class WhereValuesHashTest < Test::Unit::TestCase
       test 'includes translated attributes' do
         where_values = User.with_translations(:en).where(:email => 'foo@example.com', :user_translations => {:name => 'test_name'}).where_values_hash
 
