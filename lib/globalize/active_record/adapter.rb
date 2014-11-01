8daecd29c7df2f42db1e2297d7a709d6ec78f1e7
Nico <nico.ritsche@gmail.com>
2014-11-01 23:14:55 +0200

    attempt to make it work with rails 4.2

--- lib/globalize/active_record/adapter.rb
+++ lib/globalize/active_record/adapter.rb
@@ -54,7 +54,7 @@ module Globalize
       # Sometimes the translation is initialised before a foreign key can be set.
       def ensure_foreign_key_for(translation)
         # AR >= 4.1 reflections renamed to _reflections
-        translation[translation.class.reflections[:globalized_model].foreign_key] = record.id
+        translation[translation.class.reflections["globalized_model"].foreign_key] = record.id
       end
 
       def type_cast(name, value)
