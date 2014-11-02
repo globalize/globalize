ce624c89d3ca7034501955832676e4e74f522ddc
Nico <nico.ritsche@gmail.com>
2014-11-02 14:27:00 +0200

    rails 4.2 needs table_name= above translates call for some reason

--- test/data/models/news_item.rb
+++ test/data/models/news_item.rb
@@ -1,4 +1,5 @@
 class NewsItem < ActiveRecord::Base
-  self.table_name = 'news'
+  self.table_name = :news if Globalize::RAILS_4_2
   translates :title, :foreign_key => :news_id
+  self.table_name = :news unless Globalize::RAILS_4_2
 end
