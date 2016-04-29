9a27a3ab3daf13ade3712da8dc71d70e47dfb2d8
Nicola Prando <nicola.prando@intesys.it>
2016-04-29 10:34:17 +0200

    Removed deprecation on reset_*!. Rails 4.2 already use restore_*!

--- lib/globalize/active_record/adapter_dirty.rb
+++ lib/globalize/active_record/adapter_dirty.rb
@@ -13,7 +13,7 @@ module Globalize
           # If there's already a change, delete it if this undoes the change.
           old_values.delete locale
           if old_values.empty?
-            _reset_attribute name
+            record.restore_attributes [name]
           end
         elsif !is_changed
           # If there's not a change yet, record it.
@@ -38,15 +38,6 @@ module Globalize
       end
       def clear_dirty
         self.dirty = {}
-      end
-
-      RESET_METHOD = if defined?(::Rails::VERSION::STRING)
-        ::Rails::VERSION::STRING.split('.').first.to_i > 4 ? :restore_attribute! : :reset_attribute!
-      else
-        :reset_attribute!
-      end
-      def _reset_attribute name
-        record.send(RESET_METHOD, name) if record.respond_to? RESET_METHOD, true
       end
 
       def reset
