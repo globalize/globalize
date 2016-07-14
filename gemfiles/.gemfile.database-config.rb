22eab9bcf7af4776043fee6df6a283b4ad458cd6
Philip Arndt <git@p.arndt.io>
2016-07-14 22:17:20 +1200

    Allow JRuby CI to even function

--- gemfiles/.gemfile.database-config.rb
+++ gemfiles/.gemfile.database-config.rb
@@ -1,19 +1,22 @@
 # Database Configuration
 if !ENV['TRAVIS'] || ENV['DB'] == 'sqlite3'
-  gem 'activerecord-jdbcsqlite3-adapter', '~> 1.3.14', platform: :jruby
+  gem 'activerecord-jdbcsqlite3-adapter', github: "jruby/activerecord-jdbc-adapter", branch: "rails-5",
+      platform: :jruby
   gem 'sqlite3', platforms: [:ruby, :rbx]
 end
 
 if !ENV['TRAVIS'] || ENV['DB'] == 'mysql'
   group :mysql do
-    gem 'activerecord-jdbcmysql-adapter', '~> 1.3.14', platform: :jruby
+    gem 'activerecord-jdbcmysql-adapter', github: "jruby/activerecord-jdbc-adapter", branch: "rails-5",
+      platform: :jruby
     gem 'mysql2', platforms: [:ruby, :rbx]
   end
 end
 
 if !ENV['TRAVIS'] || %w(postgres postgresql).include?(ENV['DB'])
   group :postgres, :postgresql do
-    gem 'activerecord-jdbcpostgresql-adapter', '~> 1.3.14', platform: :jruby
+    gem 'activerecord-jdbcpostgresql-adapter', github: "jruby/activerecord-jdbc-adapter", branch: "rails-5",
+      platform: :jruby
     gem 'pg', platforms: [:ruby, :rbx]
   end
 end
