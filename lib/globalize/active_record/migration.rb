9e61d96942ba8bb9db1815a6aab52f221faead8a
Juanma <juanma@eelp.com>
2016-08-09 18:56:51 +0200

    first try of citext support for postgresql

--- lib/globalize/active_record/migration.rb
+++ lib/globalize/active_record/migration.rb
@@ -153,7 +153,7 @@ module Globalize
         end
 
         def valid_field_type?(name, type)
-          !translated_attribute_names.include?(name) || [:string, :text].include?(type)
+          !translated_attribute_names.include?(name) || [:string, :text, :citext].include?(type)
         end
 
         def translation_index_name
