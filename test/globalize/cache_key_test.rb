431f474ad3ca9dc4230778d2e8b80d6d584328a2
Andrea Dal Ponte <info@andreadalponte.com>
2016-08-23 17:01:31 +0200

    Fix: replace the test sleeping logic with time travel helpers

--- test/globalize/cache_key_test.rb
+++ test/globalize/cache_key_test.rb
@@ -1,16 +1,17 @@
 # encoding: utf-8
 require File.expand_path('../../test_helper', __FILE__)
+require 'active_support/testing/time_helpers'
 
 class CacheKeyTest < MiniTest::Spec
+  include ActiveSupport::Testing::TimeHelpers
+
   describe '#cache_key' do
     it "changes when the translation is updated" do
+      travel_to(1.day.ago)
       product = with_locale(:en) { Product.create(:name => 'first') }
       original_cache_key = product.cache_key
 
-      # If the test is too fast the new cache_key (based on the `updated_at`
-      # column) will be equal, causing a failing test.
-      sleep(1)
-
+      travel_back
       product_translation = product.translation_for(:en)
       product_translation.name = "second"
       product_translation.save!
