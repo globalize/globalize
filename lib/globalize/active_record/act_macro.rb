3fce05389fb4bf76883e3fdfd2c5e36b0db6d3ff
Chris Salzberg <chris@dejimata.com>
2016-10-02 23:07:24 +0900

    Modify wording to indicate we previously supported this configuration
    [ci skip]

--- lib/globalize/active_record/act_macro.rb
+++ lib/globalize/active_record/act_macro.rb
@@ -53,7 +53,7 @@ module Globalize
         if (overlap = attr_names.map(&:to_s) & column_names).present?
           ActiveSupport::Deprecation.warn(
             ["You have defined translated attributes with names that conflict with columns on the model table. ",
-             "Globalize does not support this configuration, remove or rename these columns.\n",
+             "Globalize does not support this configuration anymore, remove or rename these columns.\n",
              "Model name (table name): #{model_name} (#{table_name})\n",
              "Attribute name(s): #{overlap.join(', ')}\n"].join
           )
