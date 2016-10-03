c711a237b6ff5f9c34f08d27b1387b07469ed794
Chris Salzberg <chris@dejimata.com>
2016-10-03 23:11:09 +0900

    Revert "separate translated attributes"
    This reverts commit a272dc5fdc5a8876f86fcf64ba101a86485e42a0.

--- lib/globalize/active_record/query_methods.rb
+++ lib/globalize/active_record/query_methods.rb
@@ -14,13 +14,11 @@ module Globalize
       def where(opts = :chain, *rest)
         if opts == :chain
           WhereChain.new(spawn)
+        elsif parsed = parse_translated_conditions(opts)
+          #join_translations(super(parsed, *rest))
+          join_translations(super(parsed, *rest), opts)
         else
-          translated, normal = translated_vs_normal(opts)
-          if not translated.empty?
-            join_translations(super(normal, *rest), translated)
-          else
-            super
-          end
+          super
         end
       end
 
@@ -38,17 +36,6 @@ module Globalize
         else
           super
         end
-      end
-
-      def translated_vs_normal(opts)
-        translated = {}
-        if opts.is_a?(Hash) && respond_to?(:translated_attribute_names) && (opts.symbolize_keys.keys & translated_attribute_names).present?
-          opts = opts.dup
-          opts.each do |k,v|
-              translated[k] = opts.delete(k) || opts.delete(key.to_s) if translated_column? k.to_sym
-          end
-        end
-        return translated, opts
       end
 
       def with_translations_in_fallbacks
