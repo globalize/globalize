525352dff51ecd1f6384c78e59b2e3850723d6e3
Philip Arndt <git@p.arndt.io>
2017-03-03 10:23:58 +1300

    Add expectation to test.

--- test/globalize/dont_modify_immutable_relation_test.rb
+++ test/globalize/dont_modify_immutable_relation_test.rb
@@ -4,8 +4,10 @@ require File.expand_path('../../test_helper', __FILE__)
 class DontModifyImmutableRelationTest < MiniTest::Spec
   describe 'going through has_many :through relation and where.not condition' do
     it "does not raises ActiveRecord::ImmutableRelation" do
-      # This should not raise ActiveRecord::ImmutableRelation
-      Author.new.comments_without_translations.where.not(content: 'anything really')
+      expect {
+        Author.new.comments_without_translations
+              .where.not(content: 'anything really')
+      }.not_to raise_exception(::ActiveRecord::ImmutableRelation)
     end
   end
 end
