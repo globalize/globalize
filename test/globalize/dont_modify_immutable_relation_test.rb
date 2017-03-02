2fd4270b322f2dbc619441aacdfc8352caa183a9
Philip Arndt <git@p.arndt.io>
2017-03-03 10:49:35 +1300

    Revert "Add expectation to test."
    This reverts commit 525352dff51ecd1f6384c78e59b2e3850723d6e3.
    
    I thought this was rspec, but it isn't.  Aja has a good blog post about
    why this isn't needed:
    http://www.thagomizer.com/blog/2013/06/11/assert-nothing-raised.html

--- test/globalize/dont_modify_immutable_relation_test.rb
+++ test/globalize/dont_modify_immutable_relation_test.rb
@@ -4,10 +4,8 @@ require File.expand_path('../../test_helper', __FILE__)
 class DontModifyImmutableRelationTest < MiniTest::Spec
   describe 'going through has_many :through relation and where.not condition' do
     it "does not raises ActiveRecord::ImmutableRelation" do
-      expect {
-        Author.new.comments_without_translations
-              .where.not(content: 'anything really')
-      }.not_to raise_exception(::ActiveRecord::ImmutableRelation)
+      # This should not raise ActiveRecord::ImmutableRelation
+      Author.new.comments_without_translations.where.not(content: 'anything really')
     end
   end
 end
