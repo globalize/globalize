a3d8eeedf0ef378f0a17b33b618f385ced9e59a4
Bram Jetten <mail@bramjetten.nl>
2017-05-17 11:44:23 +0200

    Add patch for uniqueness validator in Rails 5.1. Removed unused table attribute.

--- lib/patches/active_record/uniqueness_validator.rb
+++ lib/patches/active_record/uniqueness_validator.rb
@@ -1,5 +1,7 @@
 if ::ActiveRecord::VERSION::STRING < "5.0.0"
   require_relative 'rails4/uniqueness_validator'
-else
+elsif ::ActiveRecord::VERSION::STRING < "5.1.0"
   require_relative 'rails5/uniqueness_validator'
+else
+  require_relative 'rails5_1/uniqueness_validator'
 end
