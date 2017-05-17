30f7292cade2f838ff5e59fa22456b1a4e7b6982
Bram Jetten <mail@bramjetten.nl>
2017-05-17 13:33:48 +0200

    Rescue ActiveRecord::NoDatabaseError instead of calling connected?

--- lib/globalize/active_record/act_macro.rb
+++ lib/globalize/active_record/act_macro.rb
@@ -40,15 +40,18 @@ module Globalize
           self.translated_attribute_names << attr_name
         end
 
-        if ::ActiveRecord::VERSION::STRING > "5.0" && connected? && table_exists? && translation_class.table_exists?
-          self.ignored_columns += translated_attribute_names.map(&:to_s)
-          reset_column_information
+        begin
+          if ::ActiveRecord::VERSION::STRING > "5.0" && table_exists? &&translation_class.table_exists?
+            self.ignored_columns += translated_attribute_names.map(&:to_s)
+            reset_column_information
+          end
+        rescue ::ActiveRecord::NoDatabaseError
         end
       end
 
       def check_columns!(attr_names)
         # If tables do not exist or Rails version is greater than 5, do not warn about conflicting columns
-        return unless ::ActiveRecord::VERSION::STRING < "5.0" && connected? && table_exists? && translation_class.table_exists?
+        return unless ::ActiveRecord::VERSION::STRING < "5.0" && table_exists? && translation_class.table_exists?
         if (overlap = attr_names.map(&:to_s) & column_names).present?
           ActiveSupport::Deprecation.warn(
             ["You have defined one or more translated attributes with names that conflict with column(s) on the model table. ",
@@ -57,6 +60,7 @@ module Globalize
              "Attribute name(s): #{overlap.join(', ')}\n"].join
           )
         end
+      rescue ::ActiveRecord::NoDatabaseError
       end
 
       def apply_globalize_options(options)
