e946e63983d42c9ff49b39b13ffcc3ad45626dd3
itschn <lofd23@yahoo.de>
2017-07-18 16:45:36 +0200

    Resolve deprecation warning: attribute is not an attribute known to Active Record.

--- lib/globalize/active_record/class_methods.rb
+++ lib/globalize/active_record/class_methods.rb
@@ -98,6 +98,7 @@ module Globalize
       end
 
       def define_translated_attr_accessor(name)
+        attribute(name)
         define_translated_attr_reader(name)
         define_translated_attr_writer(name)
       end
