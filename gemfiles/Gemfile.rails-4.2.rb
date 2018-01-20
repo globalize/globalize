3d38a1ab74609d0848eedb76638aa970f1a968c0
ota42y <ota42y@gmail.com>
2018-01-20 17:00:38 +0900

    :bug: jruby test CI fix

--- gemfiles/Gemfile.rails-4.2.rb
+++ gemfiles/Gemfile.rails-4.2.rb
@@ -16,14 +16,14 @@ end
 
 platforms :jruby do
   if !ENV['TRAVIS'] || ENV['DB'] == 'sqlite3'
-    gem 'activerecord-jdbcsqlite3-adapter'
+    gem 'activerecord-jdbcsqlite3-adapter', '~> 1'
   end
 
   if !ENV['TRAVIS'] || ENV['DB'] == 'mysql'
-    gem 'activerecord-jdbcmysql-adapter'
+    gem 'activerecord-jdbcmysql-adapter', '~> 1'
   end
 
   if !ENV['TRAVIS'] || %w(postgres postgresql).include?(ENV['DB'])
-    gem 'activerecord-jdbcpostgresql-adapter'
+    gem 'activerecord-jdbcpostgresql-adapter', '~> 1'
   end
 end
