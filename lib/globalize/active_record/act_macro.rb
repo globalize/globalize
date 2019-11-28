6934c91ecc351f5c392c05f4847ce7a7bb8a0761
Jonathan Reyes <me@jonathanreyes.com>
2019-11-28 09:08:44 +0200

    Removing the database checks and ignore columns for initialization

--- lib/globalize/active_record/act_macro.rb
+++ lib/globalize/active_record/act_macro.rb
@@ -5,7 +5,6 @@ module Globalize
         options = attr_names.extract_options!
         # Bypass setup_translates! if the initial bootstrapping is done already.
         setup_translates!(options) unless translates?
-        check_columns!(attr_names)
 
         # Add any extra translatable attributes.
         attr_names = attr_names.map(&:to_sym)
@@ -39,30 +38,6 @@ module Globalize
           # Add attribute to the list.
           self.translated_attribute_names << attr_name
         end
-
-        begin
-          if ::ActiveRecord::VERSION::STRING > "5.0" && table_exists? && translation_class.table_exists?
-            self.ignored_columns += translated_attribute_names.map(&:to_s)
-            reset_column_information
-          end
-        rescue ::ActiveRecord::NoDatabaseError
-          warn 'Unable to connect to a database. Globalize skipped ignoring columns of translated attributes.'
-        end
-      end
-
-      def check_columns!(attr_names)
-        # If tables do not exist or Rails version is greater than 5, do not warn about conflicting columns
-        return unless ::ActiveRecord::VERSION::STRING < "5.0" && table_exists? && translation_class.table_exists?
-        if (overlap = attr_names.map(&:to_s) & column_names).present?
-          ActiveSupport::Deprecation.warn(
-            ["You have defined one or more translated attributes with names that conflict with column(s) on the model table. ",
-             "Globalize does not support this configuration anymore, remove or rename column(s) on the model table.\n",
-             "Model name (table name): #{model_name} (#{table_name})\n",
-             "Attribute name(s): #{overlap.join(', ')}\n"].join
-          )
-        end
-      rescue ::ActiveRecord::NoDatabaseError
-        warn 'Unable to connect to a database. Globalize skipped checking attributes with conflicting column names.'
       end
 
       def apply_globalize_options(options)
