9f0f858745d2321e50ca3fb06ffd05c5004e244d
Alex <alex.korsak@gmail.com>
2022-05-24 03:47:35 +0200

    Changed serialize method in Rails 6.1.0+ (#798)
    Rails 6.1.0 has an extra argument for `serialize` which is `**options`.
    
    [https://api.rubyonrails.org/v6.1.0/classes/ActiveRecord/AttributeMethods/Serialization/ClassMethods.html#method-i-serialize](https://api.rubyonrails.org/v6.1.0/classes/ActiveRecord/AttributeMethods/Serialization/ClassMethods.html#method-i-serialize)
    
    `serialize(attr_name, class_name_or_coder = Object, **options)`

--- lib/patches/active_record/serialization.rb
+++ lib/patches/active_record/serialization.rb
@@ -1,5 +1,7 @@
 if ::ActiveRecord.version < Gem::Version.new("5.1.0")
   require_relative 'rails4/serialization'
-else
+elsif ::ActiveRecord.version < Gem::Version.new("6.1.0")
   require_relative 'rails5_1/serialization'
+else
+  require_relative 'rails6_1/serialization'
 end
